5e71061c03cff08c21703ceb6537ab437cd76032
Tyler Deitz <tylerdeitz@gmail.com>
2023-04-07 16:07:40 -0700

    test

---
+++ pages/subscribe.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+  return (
+    <iframe width={400} height={300} frameBorder={0} scrolling="no" src="https://cannedsoup.mirror-preview.com/subscribe/embed" />
+  )
+}
